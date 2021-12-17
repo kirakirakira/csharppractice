39e567ba56bff1c2f7dfa95e7f3d8dafed84058f
Kira Hartlage <kira.hartlage@gmail.com>
2021-12-17 14:54:18 -0500

    practice program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+for (var n = 0; n < args.Length; n++)
+{
+    Console.WriteLine($"args[{n}] = {args[n]}");
+}
+
+return 0;
