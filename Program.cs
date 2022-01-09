0ccccb1cac146a77bb45fad2b09891c2f7264fd2
Kira Hartlage <kira.hartlage@gmail.com>
2022-01-09 13:46:44 -0500

    organizing with namespaces

--- Program.cs
+++ Program.cs
@@ -1,66 +1,101 @@
-﻿using System;
-using System.Globalization;
-using System.Text;
+﻿using BethanysPieShopHRM.Accounting;
+using BethanysPieShopHRM.HR;
+using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace BethanysPieShopHRM
 {
-    public enum EmployeeType
-    {
-        Sales,
-        Manager,
-        Research,
-        StoreManager
-    }
-
-    enum StoreType
-    {
-        Pie = 10,
-        Donut = 20,
-        Cake = 30
-    }
     class Program
     {
+        public static List<Employee> employees = new List<Employee>();
+
+        public static void DisplayAllEmployees()
+        {
+            foreach (Employee employee in employees)
+            {
+                employee.DisplayEmployeeDetails();
+            }
+        }
         static void Main(string[] args)
         {
-            Employee bethany = new Employee(
-                "Bethany",
-                "Woody",
-                "wb@gmail.com",
-                new DateTime(1990, 1, 22),
-                25.45);
+            Console.WriteLine("Creating an employee");
+            Console.WriteLine("--------------------\n");
 
-            bethany.DisplayEmployeeDetails();
+            //Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), EmployeeType.Manager, 25);
+            Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), EmployeeType.Manager, null);
+            Employee george = new Employee("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), EmployeeType.Research, 30);
+
+            employees.Add(bethany);
+            employees.Add(george);
+
+            //Employee mysteryPerson = null;
+            //mysteryPerson.DisplayEmployeeDetails();
+
+            Customer customer = new Customer();
+
+            #region First run Bethany
+
             bethany.PerformWork();
-            bethany.StopWorking();
+            bethany.PerformWork();
+            bethany.PerformWork();
             bethany.ReceiveWage();
+            bethany.DisplayEmployeeDetails();
 
-            CalculateWage(340, EmployeeType.Sales, StoreType.Pie);
+            #endregion
+
+
+            #region First run George
+
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.ReceiveWage();
+            george.DisplayEmployeeDetails();
+
+            #endregion
+
+            Employee.taxRate = 0.05;//woohoo, less money to pay
+            #region Second run Bethany
+
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.ReceiveWage();
+            bethany.DisplayEmployeeDetails();
+
+            #endregion
+
+            #region Second run George
+
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.ReceiveWage();
+            george.DisplayEmployeeDetails();
+
+            #endregion
+
+
+            Employee.DisplayTaxRate();
+
+            Console.WriteLine("===========");
+            DisplayAllEmployees();
+            List<Employee> employees2 = new List<Employee>();
+            for (int i = 0; i < 10000000; i++)
+            {
+                Employee randomEmployee = new Employee(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), new DateTime(1979, 1, 16), EmployeeType.StoreManager, null);
+                employees2.Add(randomEmployee);
+            }
+
+            employees2.Clear();
+            employees2 = null;
+
+            // GC.Collect();
 
             Console.ReadLine();
         }
-
-        private static void CalculateWage(int baseWage, EmployeeType employeeType, StoreType storeType)
-        {
-            int calculateWage = 0;
-
-            if (employeeType == EmployeeType.Manager)
-            {
-                calculateWage = baseWage * 3;
-            }
-            else
-            {
-                calculateWage *= 2;
-            }
-
-            if (storeType == StoreType.Pie)
-            {
-                calculateWage += 500;
-            }
-
-            Console.WriteLine($"The calculated wage is {calculateWage}");
-        }
-
     }
 }
