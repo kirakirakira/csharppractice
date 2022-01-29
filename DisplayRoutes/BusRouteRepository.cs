b7a952e694ca136ed22d5a3c4b979429139283ca
Kira Hartlage <kira.hartlage@gmail.com>
2022-01-29 15:23:39 -0500

    put data into bus route repository

--- DisplayRoutes/BusRouteRepository.cs
+++ DisplayRoutes/BusRouteRepository.cs
@@ -6,34 +6,32 @@ namespace Pluralsight.ArraysCollections.Demos
 {
     public class BusRouteRepository
     {
-        // public static List<BusRoute> InitializeRoutes()
-        // {
-        //     return new List<BusRoute>
-        //     {
-        //         new BusRoute(40, new string[] { "Morecambe", "Preston" }),
-        //         new BusRoute(42, new string[] { "Lancaster", "Blackpool" }),
-        //         new BusRoute(100, new string[] { "Kira", "Bob" }),
-        //         new BusRoute(120, new string[] { "Bob", "Kira" }),
-        //         new BusRoute(1_000, new string[] { "Test origin", "Test destination" })
-        //     };
-        // }
-
-        public static SortedDictionary<int, BusRoute> InitializeRoutes()
+        private readonly BusRoute[] _allRoutes;
+        public void FindBusesTo(string location, ref BusRoute[] foundRoutes)
         {
-            BusRoute route40 = new BusRoute(40, new string[] { "Morecambe", "Preston" });
-            BusRoute route42 = new BusRoute(42, new string[] { "Lancaster", "Blackpool" });
-            BusRoute route100 = new BusRoute(100, new string[] { "Kira", "Bob" });
-            BusRoute route120 = new BusRoute(120, new string[] { "Bob", "Kira" });
-            BusRoute route1_000 = new BusRoute(1_000, new string[] { "Test origin", "Test destination" });
+            try
+            {
+                BusRoute[] returnedRoutes = Array.FindAll(_allRoutes,
+                    route => route.Serves(location));
 
-            var routes = new SortedDictionary<int, BusRoute>();
-            routes.Add(42, route42);
-            routes.Add(40, route40);
-            routes.Add(1000, route1_000);
-            routes.Add(100, route100);
-            routes.Add(120, route120);
+                foundRoutes = returnedRoutes;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.Write("Exception thrown: ");
 
-            return routes;
+                Console.Write("{0}", e.GetType(), e.Message);
+            }
+        }
+
+        public BusRouteRepository()
+        {
+            _allRoutes = new BusRoute[] {
+                new BusRoute(40, new string[] { "Morecambe", "Preston" }),
+                new BusRoute(42, new string[] { "Lancaster", "Blackpool" }),
+                new BusRoute(100, new string[] { "Kira", "Bob" }),
+                new BusRoute(120, new string[] { "Bob", "Kira" })
+            };
         }
     }
 }
--- DisplayRoutes/Program.cs
+++ DisplayRoutes/Program.cs
@@ -6,65 +6,26 @@ namespace Pluralsight.ArraysCollections.Demos
 {
     class Program
     {
-        public static void FindBusesTo(BusRoute[] routes, string location, ref BusRoute[] foundRoutes)
-        {
-            try
-            {
-                BusRoute[] returnedRoutes = Array.FindAll(routes,
-                    route => route.Serves(location));
-
-                foundRoutes = returnedRoutes;
-            }
-            catch (NullReferenceException e)
-            {
-                Console.Write("Exception thrown: ");
-
-                Console.Write("{0}", e.GetType(), e.Message);
-            }
-        }
         static void Main(string[] args)
         {
-            var routes = BusRouteRepository.InitializeRoutes();
+            BusRouteRepository repository = new BusRouteRepository();
 
-            foreach (BusRoute route in routes.Values)
+            Console.WriteLine("Where do you want to go?");
+            string location = Console.ReadLine();
+            BusRoute[] foundRoutes = new BusRoute[5];
+            repository.FindBusesTo(location, ref foundRoutes);
+
+            if (foundRoutes.Length > 0)
             {
-                Console.WriteLine(route);
+                foreach (BusRoute route in foundRoutes)
+                {
+                    Console.WriteLine($"You can use route {route}");
+                }
             }
-            // BusRoute foundRoute = routes[40];
-            // Console.WriteLine(foundRoute);
-
-            // int number = 25;
-
-            // // bool success = routes.TryGetValue(number, out BusRoute? answer);
-            // bool success = routes.ContainsKey(number);
-
-            // if (success)
-            // {
-            //     Console.WriteLine($"The route you asked for is {routes[number]}");
-            // }
-            // else
-            // {
-            //     Console.WriteLine($"There is no route with number {number}");
-            // }
-            // foreach (BusRoute route in routes)
-            //     Console.WriteLine(route);
-
-            // routes.RemoveAll(route => route.Origin.StartsWith("Test "));
-
-            // foreach (BusRoute route in routes)
-            //     Console.WriteLine(route);
-
-
-            // BusRoute[] foundRoutes = new BusRoute[5];
-            // FindBusesTo(routes, "Kira", ref foundRoutes);
-
-            // if (foundRoutes != null)
-            //     foreach (BusRoute route in foundRoutes)
-            //         Console.WriteLine(route);
-            // else
-            //     Console.WriteLine("No routes");
-
-
+            else
+            {
+                Console.WriteLine($"No routes go to {location}");
+            }
         }
     }
 }
